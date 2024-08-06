d90e0c38b1008088cc2552371237ce800161abb1
Felipe <felipepozo.dev@gmail.com>
2024-08-06 07:11:07 -0300

    click test js

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const $ = (el) => document.querySelector(el)
 
 const table = $('.table')
 
+let switchBlock = 0
+
 table.addEventListener('click', (square) => {
   const target = square.target
+
+  console.log(target, square)
+
+  if (!target.classList.contains('select-blue') && !target.classList.contains('select-sky-blue')) {
+    if (switchBlock === 0) {
+      target.classList.add('select-blue')
+      switchBlock++
+    } else {
+      target.classList.add('select-sky-blue')
+      switchBlock--
+    }
+  }
 })
