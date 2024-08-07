36becdc4f1f473e2f57a0092774af3b1f4d07a23
Felipe <felipepozo.dev@gmail.com>
2024-08-07 07:47:44 -0300

    refactorizacion

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
 import { TABLE } from './table.js'
+// import { T } from './squares.js'
 
-console.log(TABLE)
-
-const $ = (el) => document.querySelectorAll(el)
+const $ = (el) => document.querySelector(el)
 
 const table = $('.table')
 
-const div = $('.div-square')
-
 let switchBlock = 0
 
-div.forEach((divs, index) => {
-  const span = divs.querySelectorAll('.square')
+function load () {
+  let idCols = 0
+  let idRows
+  for (const row of TABLE) {
+    if (idRows === undefined) idRows = 0
+    else {
+      idRows++
+    }
+    // eslint-disable-next-line no-unused-vars
+    idCols = 0
 
-  span.forEach((span, spanIndex) => {
-    span.addEventListener('click', (square) => {
-      const target = square.target
-      if (!target.classList.contains('select-blue') && !target.classList.contains('select-sky-blue')) {
-        if (switchBlock === 0) {
-          target.classList.add('select-blue')
-          switchBlock++
-        } else {
-          target.classList.add('select-sky-blue')
-          switchBlock--
-        }
-      }
-      changeArray(index, spanIndex)
-    })
-  })
-})
+    for (const cols of row) {
+      const span = document.createElement('span')
+      if (!span.getAttribute('id-rows')) span.setAttribute('idRows', idRows)
+      span.classList.add('square')
+      span.classList.add('second-bg-table')
 
-function changeArray (index, spanIndex) {
-  TABLE[index][spanIndex] = '1'
-  console.table(TABLE)
+      span.setAttribute('idCols', idCols)
+      idCols++
+      table.appendChild(span)
+    }
+  }
 }
+
+load()
+
+function upload ({ rows, column }) {
+  TABLE[rows][column] = '1'
+  console.log(TABLE)
+
+  for (const row of TABLE) {
+    for (const cols of row) {
+
+    }
+  }
+}
+
+table.addEventListener('click', (el) => {
+  const target = el.target
+  const { idrows, idcols } = target.attributes
+  if (!target.classList.contains('select-blue') && !target.classList.contains('select-sky-blue')) {
+    if (switchBlock === 0) {
+      target.classList.add('select-blue')
+      switchBlock++
+    } else {
+      target.classList.add('select-sky-blue')
+      switchBlock--
+    }
+  }
+  upload({ column: Number(idcols.value), rows: Number(idrows.value) })
+})
