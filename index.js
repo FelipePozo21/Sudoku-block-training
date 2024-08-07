345f3b3064f752254997bd2c2d5cef03d33d4e31
Felipe <felipepozo.dev@gmail.com>
2024-08-07 13:15:29 -0300

    add remore row

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { TABLE } from './table.js'
 // import { T } from './squares.js'
 
-const $ = (el) => document.querySelector(el)
+const $ = (el) => document.querySelectorAll(el)
 
 const table = $('.table')
 
@@ -15,9 +15,9 @@ function load () {
     else {
       idRows++
     }
-    // eslint-disable-next-line no-unused-vars
     idCols = 0
 
+    // eslint-disable-next-line no-unused-vars
     for (const cols of row) {
       const span = document.createElement('span')
       if (!span.getAttribute('id-rows')) span.setAttribute('idRows', idRows)
@@ -26,7 +26,7 @@ function load () {
 
       span.setAttribute('idCols', idCols)
       idCols++
-      table.appendChild(span)
+      table[0].appendChild(span)
     }
   }
 }
@@ -35,26 +35,48 @@ load()
 
 function upload ({ rows, column }) {
   TABLE[rows][column] = '1'
-  console.log(TABLE)
 
-  for (const row of TABLE) {
-    for (const cols of row) {
-
-    }
+  const test = TABLE[rows].every(item => item === '1')
+  console.log({ test, TABLE })
+  if (test) {
+    return removeRow(rows.toString())
   }
+  return TABLE
+  // for (const row of TABLE) {
+  //   const someRow = row.every(el => el === '1')
+  //   if (someRow) {
+  //     removeRow(rows.toString())
+  //   }
+  // }
 }
 
-table.addEventListener('click', (el) => {
+table[0].addEventListener('click', (el) => {
   const target = el.target
   const { idrows, idcols } = target.attributes
-  if (!target.classList.contains('select-blue') && !target.classList.contains('select-sky-blue')) {
-    if (switchBlock === 0) {
-      target.classList.add('select-blue')
-      switchBlock++
-    } else {
-      target.classList.add('select-sky-blue')
-      switchBlock--
+
+  upload({ column: Number(idcols.value), rows: Number(idrows.value) })
+  if (TABLE[Number(idrows.value)][Number(idcols.value)] === '1') {
+    if (!target.classList.contains('select-blue') && !target.classList.contains('select-sky-blue')) {
+      if (switchBlock === 0) {
+        target.classList.add('select-blue')
+        switchBlock++
+      } else {
+        target.classList.add('select-sky-blue')
+        switchBlock--
+      }
     }
   }
-  upload({ column: Number(idcols.value), rows: Number(idrows.value) })
+  return table
 })
+
+function removeRow (index) {
+  TABLE[index] = ['0', '0', '0', '0', '0', '0', '0', '0', '0']
+  const row = document.querySelectorAll(`span[idrows="${index}"]`)
+  row.forEach(span => {
+    if (span.classList.contains('select-blue')) {
+      span.classList.remove('select-blue')
+    } else {
+      span.classList.remove('select-sky-blue')
+    }
+  })
+}
