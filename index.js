ddf7dcdd66afc4145a8d0d7f44db92cd80aa7edf
Felipe <felipepozo.dev@gmail.com>
2024-08-08 17:10:00 -0300

    finish por hoy

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import { BLOCKS } from './squares.js'
 import { TABLE } from './table.js'
-// import { T } from './squares.js'
 
 const $ = (el) => document.querySelectorAll(el)
 
 const table = $('.table')
 
 const preview = $('.preview-block')
+
+let targetBlockSelect
 
 let switchBlock = 0
 
@@ -48,11 +49,11 @@ function upload ({ rows, column }) {
   return TABLE
 }
 
-table[0].addEventListener('click', (el) => {
+table[0].addEventListener('mousedown', (el) => {
   const target = el.target
-  const { idrows, idcols } = target.attributes
-
-  upload({ column: Number(idcols.value), rows: Number(idrows.value) })
+  const { idrows, idcols } = target?.attributes
+  targetBlockSelect = { rows: Number(idrows?.value), cols: Number(idcols?.value) }
+  upload({ column: Number(idcols?.value), rows: Number(idrows?.value) })
   if (TABLE[Number(idrows.value)][Number(idcols.value)] === '1') {
     if (!target.classList.contains('select-blue') && !target.classList.contains('select-sky-blue')) {
       if (switchBlock === 0) {
@@ -80,8 +81,7 @@ function removeRow (index) {
 }
 
 function squares () {
-  BLOCKS.forEach((item, index) => {
-    console.log({ item, index })
+  BLOCKS.forEach(item => {
     const div = document.createElement('div')
     div.addEventListener('click', () => selectedBlock(item))
     div.classList.add('preview-grid')
@@ -89,7 +89,6 @@ function squares () {
       for (const square of squares) {
         const span = document.createElement('span')
         span.style.outline = 'none'
-        console.log(square)
         span.classList.add('square')
         if (square === 1) {
           span.classList.add('select-blue')
@@ -103,8 +102,21 @@ function squares () {
 squares()
 
 function selectedBlock (square) {
-  blocksToInsert = undefined
   blocksToInsert = square
-
+  // let { rows, cols } = targetBlockSelect ?? ''
+  // for (const arr of blocksToInsert) {
+  //   for (const block of arr) {
+  //     console.log({ block })
+  //     if (arr[0] && block[0]) {
+  //       TABLE[rows][cols] = '1'
+  //     }
+  //     if (block === '1') {
+  //       TABLE[rows][cols + 1] = '1'
+  //       cols++
+  //     }
+  //     const test = TABLE[rows][cols + 1] ?? 's'
+  //     console.log(test)
+  //   }
+  // }
   console.log(blocksToInsert)
 }
